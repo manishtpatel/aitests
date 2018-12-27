d1397889dd48803d3d122be586921676fbd4f600
Manish Patel <manishtpatel@yahoo.com>
2018-12-26 22:03:20 -0500

    save

--- regression.py
+++ regression.py
@@ -1,1 +1,151 @@
-print("test")
+# %% Setup Data
+from __future__ import absolute_import, division, print_function
+import matplotlib.pyplot as plt
+import pathlib
+
+import pandas as pd
+import seaborn as sns
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# download dataset
+dataset_path = keras.utils.get_file(
+    'auto-mpg.data', 'https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data')
+print(dataset_path)
+
+column_names = ['MPG', 'Cylinders', 'Displacement',
+                'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin']
+
+raw_dataset = pd.read_csv(dataset_path, names=column_names,
+                          na_values='?', comment='\t', sep=' ', skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+
+# clean data
+dataset = dataset.dropna()
+# one-hot origin
+origin = dataset.pop('Origin')
+dataset['USA'] = (origin == 1)*1.0
+dataset['Europe'] = (origin == 2)*1.0
+dataset['Japan'] = (origin == 3)*1.0
+
+# %% Setup Train and Test Data
+# split data into test and train
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+sns.pairplot(
+    train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind="kde")
+
+train_stats = train_dataset.describe()
+train_stats.pop('MPG')
+train_stats = train_stats.transpose()
+
+train_labels = train_dataset.pop('MPG')
+test_labels = test_dataset.pop('MPG')
+
+# %% Normalize the data
+
+
+def norm(x):
+    return (x - train_stats['mean']) / train_stats['std']
+
+
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
+
+# %% Build model
+
+
+def build_model():
+    model = keras.Sequential([
+        layers.Dense(64, activation=tf.nn.relu, input_shape=[
+                     len(train_dataset.keys())]),
+        layers.Dense(64, activation=tf.nn.relu),
+        layers.Dense(1)
+    ])
+
+    optimizer = tf.train.RMSPropOptimizer(0.001)
+
+    model.compile(loss='mse', optimizer=optimizer, metrics=['mae', 'mse'])
+
+    return model
+
+
+model = build_model()
+
+model.summary()
+
+# %% Try Model
+example_batch = normed_train_data[:10]
+example_result = model.predict(example_batch)
+example_result
+
+# %% Train the model
+
+
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 100 == 0:
+            print('')
+        print('.', end='')
+
+
+EPOCHS = 1000
+
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
+
+history = model.fit(normed_train_data, train_labels, epochs=EPOCHS,
+                    validation_split=0.2, verbose=0, callbacks=[early_stop, PrintDot()])
+
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+hist.tail()
+
+# plot history
+
+
+def plot_history(history):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error [MPG]')
+    plt.plot(hist['epoch'], hist['mean_absolute_error'], label='Train Error')
+    plt.plot(hist['epoch'], hist['val_mean_absolute_error'], label='Val Error')
+    plt.legend()
+    plt.ylim([0, 5])
+
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Square Error [$MPG^2$]')
+    plt.plot(hist['epoch'], hist['mean_squared_error'], label='Train Error')
+    plt.plot(hist['epoch'], hist['val_mean_squared_error'], label='Val Error')
+    plt.legend()
+    plt.ylim([0, 20])
+
+
+plot_history(history)
+
+# %% Check with test data
+# how did model did? check with test dataset
+loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=0)
+
+mae
+
+# %% Make Predictions
+test_predictions = model.predict(normed_test_data).flatten()
+
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [MPG]')
+plt.ylabel('Predictions [MPG]')
+plt.axis('equal')
+plt.axis('square')
+plt.xlim(0, plt.xlim()[1])
+plt.ylim(0, plt.ylim()[1])
+_ = plt.plot([-100, 100], [-100, 100])
+
+error = test_predictions - test_labels
+plt.hist(error, bins=25)
+plt.xlabel('Predictions Error [MPG]')
+_ = plt.ylabel("Count")
